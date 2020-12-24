c75a102c4fc0e0034323ee42b1f0b06b48ef1899
IllusionzDev <68855711+IllusionzDev@users.noreply.github.com>
2020-12-24 00:22:45 +0000

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,40 @@
+public final class Main extends JavaPlugin {
+    private Economy eco;
+    public static Main instance = null;
+    public static CreateConfig config = null;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        Class.loadClasses();
+       if (!setupEconomy()) {
+            this.getLogger().severe("Plugin disabled due to no Vault dependency found!");
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
+       }
+       config = new CreateConfig("plugins/Duels", "config.yml", instance);
+        getCommand("nightarena").setExecutor(new DuelCommands());
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    private boolean setupEconomy() {
+        if (Bukkit.getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        eco = rsp.getProvider();
+        return eco != null;
+    }
+
+    public Economy getEconomy() {
+        return eco;
+    }
+}
