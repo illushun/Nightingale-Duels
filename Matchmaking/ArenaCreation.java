7a36cc94830d041aecf153a63e4aa9921b8bf381
IllusionzDev <68855711+IllusionzDev@users.noreply.github.com>
2021-12-21 06:54:59 +0000

    Less static...

--- Matchmaking/ArenaCreation.java
+++ Matchmaking/ArenaCreation.java
@@ -2,61 +2,77 @@ public class ArenaCreation {
     public static Map<String, String> arenas = Maps.newHashMap();
     public static Map<String, Location> arenaLocation = Maps.newHashMap();
 
-    public static String getArenaCreator(String name) {
-        if (!arenas.containsKey(name)) {
-            return null;
-        }
+    private String arenaName;
 
-        return arenas.get(name);
+    public ArenaCreation() {}
+
+    public ArenaCreation(String arenaName) {
+        this.arenaName = arenaName;
     }
 
-    public static String getArenaLocation(String name) {
-        if (!arenas.containsKey(name)) {
-            return null;
-        }
-
-        return "firstPos: " + arenaLocation.get(name + "_firstPos") + " secondPos: " + arenaLocation.get(name + "_secondPos");
-    }
-
-    public static Location getArenaLocation(String name, String position) {
-        if (!arenas.containsKey(name)) {
-            return null;
-        }
-
-        switch (position) {
-            case "_firstPos":
-                return arenaLocation.get(name + "_firstPos");
-            case "_secondPos":
-                return arenaLocation.get(name + "_secondPos");
-            default:
-        }
-        return null;
-    }
-
-    public static void createArena(Player ply, String name) {
-        if (arenas.containsKey(name)) {
+    public void CreateArena(Player ply) {
+        if (arenas.containsKey(arenaName)) {
             return;
         }
 
-        arenas.put(name, ply.getName());
+        GetArenas().put(arenaName, ply.getName());
     }
 
-    public static boolean setArenaLocation(String name, String position, Location location) {
-        if (!arenas.containsKey(name)) {
-            System.out.println("There is no arena named " + name + ".");
+    public boolean SetArenaLocation(String position, Location location) {
+        if (!GetArenas().containsKey(arenaName)) {
+            System.out.println("There is no arena named " + arenaName + ".");
             return false;
         }
 
         switch (position) {
             case "firstPos":
-                arenaLocation.put(name + "_firstPos", location);
+                GetArenaLocations().put(arenaName + "_firstPos", location);
                 return true;
             case "secondPos":
-                arenaLocation.put(name + "_secondPos", location);
+                GetArenaLocations().put(arenaName + "_secondPos", location);
                 return true;
         }
 
         System.out.println("Position needs to be either 'firstPos' or 'secondPos'.");
         return false;
+    }
+
+    public Map<String, String> GetArenas() {
+        return arenas;
+    }
+
+    public Map<String, Location> GetArenaLocations() {
+        return arenaLocation;
+    }
+
+    public String GetArenaCreator() {
+        if (!GetArenas().containsKey(arenaName)) {
+            return null;
+        }
+
+        return GetArenas().get(arenaName);
+    }
+
+    public String GetArenaLocation() {
+        if (!GetArenas().containsKey(arenaName)) {
+            return null;
+        }
+
+        return "firstPos: " + GetArenaLocations().get(arenaName + "_firstPos") + " secondPos: " + arenaLocation.get(arenaName + "_secondPos");
+    }
+
+    public Location GetArenaLocation(String position) {
+        if (!GetArenas().containsKey(arenaName)) {
+            return null;
+        }
+
+        switch (position) {
+            case "_firstPos":
+                return GetArenaLocations().get(arenaName + "_firstPos");
+            case "_secondPos":
+                return GetArenaLocations().get(arenaName + "_secondPos");
+            default:
+        }
+        return null;
     }
 }
