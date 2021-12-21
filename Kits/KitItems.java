714debc2354fb0aca791d8af522fd81faf707452
IllusionzDev <68855711+IllusionzDev@users.noreply.github.com>
2021-12-21 06:59:13 +0000

    Less static...

--- Kits/KitItems.java
+++ Kits/KitItems.java
@@ -1,19 +1,29 @@
 public class KitItems {
     public static Multimap<String, ItemStack> kitItems = ArrayListMultimap.create();
 
-    public static boolean giveKitItems(Player ply, String arenaName) {
+    private Player ply;
+    private String arenaName;
+
+    public KitItems() {}
+
+    public KitItems(Player ply, String arenaName) {
+        this.ply = ply;
+        this.arenaName = arenaName;
+    }
+
+    public boolean GiveKitItems() {
         if (!CreateKit.arenaKits.contains(arenaName)) {
             System.out.println("There is no kit with the name " + arenaName + ".");
             return false;
         }
 
-        if (kitItems.isEmpty()) {
+        if (GetKitItems().isEmpty()) {
             System.out.println("Kit is empty!");
             return false;
         }
 
         try {
-            kitItems.forEach((name, items) -> {
+            GetKitItems().forEach((name, items) -> {
                 if (name.matches(arenaName)) {
                     ply.getInventory().addItem(items);
                 }
@@ -26,8 +36,8 @@ public class KitItems {
         }
     }
 
-    public static boolean addItemToKit(Player ply, String arenaName) {
-        if (!CreateKit.arenaKits.contains(arenaName)) {
+    public boolean AddItem() {
+        if (!new CreateKit().GetKits().contains(arenaName)) {
             System.out.println("There is no kit with the name " + arenaName + ".");
             return false;
         }
@@ -38,23 +48,27 @@ public class KitItems {
             return false;
         }
 
-        kitItems.put(arenaName, addItem);
+        GetKitItems().put(arenaName, addItem);
         return true;
     }
 
-    public static boolean removeItemFromKit(Player ply, String arenaName) {
-        if (!CreateKit.arenaKits.contains(arenaName)) {
+    public boolean RemoveItem() {
+        if (!new CreateKit().GetKits().contains(arenaName)) {
             System.out.println("There is no kit with the name " + arenaName + ".");
             return false;
         }
 
         ItemStack addItem = ply.getInventory().getItemInMainHand();
-        if (addItem.getType().isAir() || !kitItems.containsValue(addItem)) {
+        if (addItem.getType().isAir() || !GetKitItems().containsValue(addItem)) {
             System.out.println("That item doesn't exist for the kit " + arenaName + ".");
             return false;
         }
 
-        kitItems.remove(arenaName, addItem);
+        GetKitItems().remove(arenaName, addItem);
         return true;
+    }
+
+    public Multimap<String, ItemStack> GetKitItems() {
+        return kitItems;
     }
 }
